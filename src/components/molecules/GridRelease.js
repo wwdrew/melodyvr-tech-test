4bbe483e1b6f9848c4bf275f88622c263177660e
Drew Miller <wwdrew@users.noreply.github.com>
2019-08-15 11:11:07 +0100

    task(grid): styled the grid releases

--- src/components/molecules/GridRelease.js
+++ src/components/molecules/GridRelease.js
@@ -4,10 +4,10 @@
  */
 
 import React from 'react';
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import FreeTag from '../atoms/FreeTag';
 
-import type {ViewStyleProp} from 'StyleSheet';
+import type {TextStyleProp, ViewStyleProp} from 'StyleSheet';
 import type {Release} from '../../redux/reducers';
 
 type Props = {
@@ -17,21 +17,31 @@ type Props = {
 
 const GridRelease = ({onPress, release}: Props) => (
   <TouchableOpacity
-    style={[
-      styles.container,
-      {backgroundColor: `#${release.product.background_colour_code}`},
-    ]}
+    style={styles.container}
     onPress={() => onPress(release.feature_order)}>
-    <Text style={{color: `#${release.product.text_colour_code}`}}>
-      {release.product.title}
-    </Text>
-    {release.is_free ? (
-      <FreeTag />
-    ) : (
-      <Text style={{color: `#${release.product.text_colour_code}`}}>
-        £{release.product.price}
+    <Image
+      source={{uri: release.product.image_hero_url}}
+      style={styles.imageBackground}
+    />
+    <View
+      style={[
+        styles.overlay,
+        {backgroundColor: `#${release.product.background_colour_code}`},
+      ]}
+    />
+    <View style={styles.description}>
+      <Text
+        style={[styles.title, {color: `#${release.product.text_colour_code}`}]}>
+        {release.product.title}
       </Text>
-    )}
+      {release.is_free ? (
+        <FreeTag />
+      ) : (
+        <Text style={{color: `#${release.product.text_colour_code}`}}>
+          £{release.product.price}
+        </Text>
+      )}
+    </View>
   </TouchableOpacity>
 );
 
@@ -39,9 +49,35 @@ export default GridRelease;
 
 const styles: {
   container: ViewStyleProp,
+  imageBackground: ViewStyleProp,
+  overlay: ViewStyleProp,
+  title: TextStyleProp,
+  description: ViewStyleProp,
 } = StyleSheet.create({
   container: {
-    borderRadius: 2,
+    height: 150,
+    justifyContent: 'flex-end',
+  },
+  imageBackground: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  overlay: {
+    opacity: 0.3,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  description: {
     padding: 10,
   },
 });
