b075f6f80040078d341bd7951ddca4b3b9f37a9d
Drew Miller <wwdrew@users.noreply.github.com>
2019-08-15 12:24:11 +0100

    chore(warnings): turn off yellow box warnings

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
  * @format
  */
 
+/* Two of the packages I use cause yellow box errors, I've disabled them for
+  now so they're not distracting
+*/
+
+console.disableYellowBox = true;
+
 import {AppRegistry} from 'react-native';
 import App from './src/MelodyVRTest';
 import {name as appName} from './app.json';
